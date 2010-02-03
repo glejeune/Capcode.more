84ed102a99387f38c27fc312120b2c501c598ea6
glejeune <gregoire.lejeune@free.fr>
2010-02-03 14:55:15 +0100

    None renderer is deprecated

--- capcode-render-none/lib/capcode/render/none.rb
+++ capcode-render-none/lib/capcode/render/none.rb
@@ -1,6 +1,7 @@
 module Capcode
   module Helpers
     def render_none( f = "", _ ) #:nodoc:
+      warn( "none renderer is deprecated, please use `render( 204 => ... )'" )
       return [204, {}, f]
     end
   end
