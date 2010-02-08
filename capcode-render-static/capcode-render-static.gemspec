b61068dfd44e62abeae0a279fe6a2019d8667dd8
glejeune <gregoire.lejeune@free.fr>
2010-02-08 20:43:24 +0100

    capcode-render-static 0.3.0

--- capcode-render-static/capcode-render-static.gemspec
+++ capcode-render-static/capcode-render-static.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{capcode-render-static}
-  s.version = "0.2.0"
+  s.version = "0.3.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Gregoire Lejeune"]
-  s.date = %q{2010-02-03}
+  s.date = %q{2010-02-08}
   s.description = %q{Capcode plugin to render static file}
   s.email = %q{gregoire.lejeune@free.fr}
   s.extra_rdoc_files = [
@@ -39,9 +39,12 @@ Gem::Specification.new do |s|
     s.specification_version = 3
 
     if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
+      s.add_runtime_dependency(%q<mime-types>, [">= 0"])
     else
+      s.add_dependency(%q<mime-types>, [">= 0"])
     end
   else
+    s.add_dependency(%q<mime-types>, [">= 0"])
   end
 end
 
--- capcode-render-static/lib/capcode/render/static.rb
+++ capcode-render-static/lib/capcode/render/static.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'mime/types'
+
 module Capcode
   module Helpers
     def render_static( f, opts = {} ) #:nodoc:
@@ -5,14 +8,17 @@ module Capcode
       opts = { :exact_path => true }.merge(opts)
       opts = (Capcode::Configuration.options[:static] || {}).merge(opts)
       
-      # Update Content-Type
-      @response['Content-Type'] = opts[:content_type] if opts.keys.include?(:content_type)
-      
       # Path with ".." not allowed
       if Capcode.static.nil? or f.include? '..'
         return [403, {}, '403 - Invalid path']
       end
       
+      # Update Content-Type
+      if opts.keys.include?(:content_type)
+        @response['Content-Type'] = opts[:content_type]
+      else
+        @response['Content-Type'] = MIME::Types.type_for(File.join( static[:path], f ))[0].content_type
+      end
       
       if !opts.keys.include?(:exact_path) or opts[:exact_path] == true
         redirect File.join( static[:uri], f )
